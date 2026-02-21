0fca1edd8ac15e8181e7c3de1f3ca1ab9fc39373
meena20221515-star <meena20221515@gmail.com>
2026-02-21 14:08:05 +0530

    main

---
+++ calc/script.js
@@ -0,0 +1,26 @@
+let outscreen = document.getElementById("output-screen");
+
+function display(num){
+    outputscreen.value += num ;
+
+}
+function calculate(){
+    try{
+        outputscreen.value=eval(outputscreen.value);
+    }
+    catch (err){
+        alert("invalid");
+
+    }
+    function clear(){
+    outputscreen.value="";
+
+    }
+    function del(){
+        outputscreen.value=outputscreen.value.slice(0,-1);
+    }
+
+    
+}
+
+
